92a2cac604c693ab6c3615560c94ea220f3b9bfc
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-03-26 12:38:00 +0100

    created_error_components

---
+++ src/app/global-error.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import React from 'react';
+
+// export interface GlobalErrorProps {
+// }
+
+export default function GlobalError() {
+    return (
+        <html>
+        <body>
+        <div>
+            <p>{`Something went wrong.`}</p>
+        </div>
+        </body>
+        </html>
+
+    );
+}
