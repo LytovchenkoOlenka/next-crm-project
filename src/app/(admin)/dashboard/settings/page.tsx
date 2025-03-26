ee4a3d517feb35be11de9b64bd7f7331b695a10a
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-03-26 19:19:29 +0100

    created_new_components

--- src/app/(admin)/dashboard/settings/page.tsx
+++ src/app/(admin)/dashboard/settings/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import StatCard, {StatCardType} from "@/app/components/stat-card";
 
 // export interface PageProps {
 //     // title?: string;
@@ -8,6 +9,7 @@ export default function Page() {
     return (
         <main>
            <h1 className="text-xl">Settings Page</h1>
+            {/*<StatCard counter={13} label={'Lalala'} type={StatCardType.Dark}/>*/}
         </main>
     );
 }
