0751ea5328c8f4ade42fa141bc871e3de05b9f48
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-04-08 16:57:02 +0200

    connect_to_API

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,15 +1,12 @@
-'use client'
-
 import React from 'react';
-// import Header from '@/app/components/header';
-import { useParams } from 'next/navigation';
 
-// export interface PageProps {
-//     params: { id: string};
-// }
+export interface PageProps {
+    params: {
+        id: string;
+    };
+}
 
-export default function Page() {
-    const params = useParams();
+export default function Page({ params }: PageProps) {
     return (
         <div className="py-6 px-10">
             <p>{`Information about company (${params.id})`}</p>
